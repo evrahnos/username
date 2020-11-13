6eaf4ab9e5547e2adaac10f30475b338d79dd12a
Eleas Vrahnos <evrahnos@chapman.edu>
2020-11-13 10:50:26 -0800

    Updated username.sh

--- username.sh
+++ username.sh
@@ -3,10 +3,10 @@
 # Eleas Vrahnos
 echo "Enter a username: "
 read NAME
-while echo "$NAME" | grep -E -v "^[a-z]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][_0-9a-z]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username!"
+	echo "Enter a username: "
 	read -r NAME
 done
 echo "Thank you"

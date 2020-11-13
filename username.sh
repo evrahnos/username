69627c3326f9fb37fb116f8b79cb3f4575404cc7
Eleas Vrahnos <evrahnos@chapman.edu>
2020-11-13 11:01:40 -0800

    Updated username.sh

--- username.sh
+++ username.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # username.sh
 # Eleas Vrahnos
-echo "Enter a username: "
+echo "Enter a username. The only characters that can be used are lower case letters, digits, and the underscore character. It must start with a lower case letter. It must contain at least three but no more than twelve characters: "
 read NAME
-while echo "$NAME" | grep -E -v "^[a-z][_0-9a-z]{3,12}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][_0-9a-z]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a username: "
